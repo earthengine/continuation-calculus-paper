51d6a477981aee0c6b362cb6cf4ac69b72687a99
Bram Geron <bgeron@bgeron.nl>
2013-04-13 20:40:32 +0200

    Automaticaly 'pause' the computation after a certain number of steps, so infinite loops do not crash the browser.

--- webinclude/cc_eval.py
+++ webinclude/cc_eval.py
@@ -10,7 +10,7 @@ from pyjamas.ui.TextArea import TextArea
 from pyjamas.ui.FlowPanel import FlowPanel
 from pyjamas import Window, DOM
 
-import cc, io
+import cc, io, functools
 cc._PRINTNUM = True
 
 ncustomfiles = 0
@@ -41,8 +41,8 @@ FibCBN.x.z.s -> x.z.(FibCBN_1.z.s)
 FibCBN_1.z.s.y -> y.(s.Zero).(FibCBN_2.z.s.y)
 FibCBN_2.z.s.y.y' -> AddCBN.(FibCBN.y).(FibCBN.y').z.s
 
-# To see fib(6) = 8:
-FibCBV.(S.(S.(S.(S.(S.(S.Zero)))))).fr
+# To see fib(5) = 5:
+FibCBV.(S.(S.(S.(S.(S.Zero))))).fr
 """.strip()
 
 
@@ -80,38 +80,84 @@ def newlinesLabel(text, nlHTML="<br>"):
     return fp
 
 def showOutputMeta(text):
+    """Clear outputPanel and show information in it."""
     outputPanel.clear()
     outputPanel.add(Label(text))
     outputPanel.setStyleName("meta")
 
-def showOutput(output):
+def showOutput(output, extra=None):
+    """Clear outputPanel and show newline-separated output in it.
+
+    If extra is set, then append that widget."""
+
     output = output.strip()
     if output:
         outputPanel.clear()
         outputPanel.add(newlinesLabel(output))
+        if extra != None:
+            outputPanel.add(extra)
         # outputPanel.add(Label(output))
         outputPanel.setStyleName("proper")
     else:
         showOutputMeta("No output.")
 
-def reduce(sender):
-    """When the Reduce button is pressed: call cc.runfile with our input."""
+class OverlongOutput(Exception):
+    """Signals that we have received too much output, and we want to quit the
+    reduction computation."""
+    pass
+
+def reduce(sender=None, maxlines=300):
+    """When the Reduce button is pressed: call cc.runfile with our input.
+
+    There is a maximum number of lines that we will output, to prevent a
+    stalling browser and an overfull document. The user can raise this limit
+    with a link.
+    """
 
     showOutputMeta("Reducing...")
 
     input = inputArea.getText()
     output = ""
+    nlines = 0
     def catchoutput(s, end="\n"):
         output += s + end
+        nlines += 1
+        if nlines > maxlines:
+            raise OverlongOutput()
 
     cc._defs = dict()
     try:
         cc.runfile(inputfile=io.StringIO(input), verbose=True,
                    printout=catchoutput, printerr=catchoutput)
+    except OverlongOutput:
+        extra = FlowPanel(StyleName="terminated")
+        extra.add(InlineLabel("Reduction terminated after %s lines." % (maxlines,)))
+        extra.add(Button("Try with more time.", functools.partial(reduce, maxlines=nextmaxlines(maxlines))))
+        showOutput(output, extra=extra)
     except Exception, e:
         Window.alert(e)
+    else:
+        showOutput(output)
 
-    showOutput(output)
+def nextmaxlines(prevmaxlines):
+    """Makes the sequence 1, 3, 10, 30, 100, 300, ... . That is, when fed with
+    a number in that sequence, return the next number.
+
+    This function is used to make buttons for allowing more computation time,
+    step by step, in reduce().    
+    """
+
+    orders = 0
+    while prevmaxlines >= 10:
+        orders += 1
+        prevmaxlines /= 10
+    if prevmaxlines >= 3:
+        maxlines = 10
+    else:
+        maxlines = 3
+    for _ in range(orders):
+        maxlines *= 10
+    return maxlines
 
 if __name__ == '__main__':
     b = Button("Reduce", reduce)
---
+++ webinclude/functools.py
@@ -0,0 +1,10 @@
+def partial(func, *args, **keywords):
+    """Alternative definition of functools.partial, borrowed from the Python 2.7.3 manual."""
+    def newfunc(*fargs, **fkeywords):
+        newkeywords = keywords.copy()
+        newkeywords.update(fkeywords)
+        return func(*(args + fargs), **newkeywords)
+    newfunc.func = func
+    newfunc.args = args
+    newfunc.keywords = keywords
+    return newfunc
